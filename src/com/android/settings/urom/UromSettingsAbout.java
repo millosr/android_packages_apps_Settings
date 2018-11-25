756849d268fce850dc55d4074d4c952adc94cd6c
Milos Ratkovic <milosr@gmail.com>
2018-11-25 21:25:44 +0100

    urom: changed "About nAOSProm" screen
    Change-Id: Id8a55f1350c7ccb89d20d30e1933a3b04213c057

--- src/com/android/settings/urom/UromSettingsAbout.java
+++ src/com/android/settings/urom/UromSettingsAbout.java
@@ -16,88 +16,64 @@
 
 package com.android.settings.urom;
 
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import android.content.Intent;
-import android.net.Uri;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.SELinux;
+import android.os.SystemProperties;
 import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
+import android.util.Log;
 
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class UromSettingsAbout extends SettingsPreferenceFragment {
+    private static final String TAG = "UromSettingsAbout";
 
-    public static final String TAG = "UromSettingsAbout";
-
-    Preference mSourceUrl;
-    Preference mXdaUrl;
-    Preference mTranslationUrl;
-    Preference mDonationUrl;
-    Preference mDevs1Url;
-    Preference mDevs2Url;
-    Preference mMentions1Url;
-    Preference mMentions2Url;
-    Preference mMentions3Url;
-    Preference mMentions4Url;
-    Preference mMentions5Url;
+    private Preference mVersion;
+    private Preference mBuildDate;
+    private Preference mSelinuxStatus;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.urom_about);
 
-        mSourceUrl = findPreference("urom_source");
-        mXdaUrl = findPreference("urom_xda");
-        mTranslationUrl = findPreference("urom_translation");
-        mDonationUrl = findPreference("urom_donation");
-        mDevs1Url = findPreference("urom_devs_field1");
-        mDevs2Url = findPreference("urom_devs_field2");
-        mMentions1Url = findPreference("urom_mentions_field1");
-        mMentions2Url = findPreference("urom_mentions_field2");
-        mMentions3Url = findPreference("urom_mentions_field3");
-        mMentions4Url = findPreference("urom_mentions_field4");
-        mMentions5Url = findPreference("urom_mentions_field5");
-
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
-        if (preference == mSourceUrl) {
-            launchUrl("https://github.com/mickybart");
-        } else if (preference == mXdaUrl) {
-            launchUrl("http://forum.xda-developers.com/xperia-s/s-development/rom-naosprom-xperia-s-t2958516");
-        } else if (preference == mTranslationUrl) {
-            launchUrl("http://forum.xda-developers.com/showpost.php?p=65159347&postcount=3644");
-        } else if (preference == mDonationUrl) {
-            launchUrl("http://forum.xda-developers.com/donatetome.php?u=6043081");
-        } else if (preference == mDevs1Url) {
-            launchUrl("http://forum.xda-developers.com/member.php?u=6043081");
-        } else if (preference == mDevs2Url) {
-            launchUrl("http://forum.xda-developers.com/member.php?u=6754437");
-        } else if (preference == mMentions1Url) {
-            launchUrl("http://forum.xda-developers.com/member.php?u=5064452");
-        } else if (preference == mMentions2Url) {
-            launchUrl("http://forum.xda-developers.com/showthread.php?t=2191223");
-        } else if (preference == mMentions3Url) {
-            launchUrl("http://www.cyanogenmod.com");
-        } else if (preference == mMentions4Url) {
-            launchUrl("http://www.teamw.in");
-        } else if (preference == mMentions5Url) {
-            launchUrl("http://source.android.com/");
-        }
-        return super.onPreferenceTreeClick(preference);
-    }
-
-    private void launchUrl(String url) {
-        Uri uriUrl = Uri.parse(url);
-        Intent intentUrl = new Intent(Intent.ACTION_VIEW, uriUrl);
-        getActivity().startActivity(intentUrl);
+        mVersion = findPreference("urom_version");
+        mBuildDate = findPreference("urom_build_date");
+        mSelinuxStatus = findPreference("urom_selinux_status");
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsEvent.DISPLAY;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Resources res = getContext().getResources();
+
+        mVersion.setSummary(SystemProperties.get("ro.naosp.version"));
+        mBuildDate.setSummary(SystemProperties.get("ro.build.date"));
+        mSelinuxStatus.setSummary(getSelinuxStatus());
+    }
+
+    public String getSelinuxStatus() {
+        String status = null;
+        try {
+            Process p = Runtime.getRuntime().exec("getenforce");
+            p.waitFor();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            status = reader.readLine().trim();
+            reader.close();
+        } catch (Exception e) {
+            Log.e(TAG, "OS does not support getenforce", e);
+        }
+        return status;
     }
 }
 
